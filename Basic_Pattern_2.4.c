43e6d01698c171c832c61a6c72fab578b4644b42
SatishReddy2804 <147795536+SatishReddy2804@users.noreply.github.com>
2024-01-03 19:05:35 +0530

    #include<stdio.h>
    int main()
    {
        int i,j,k;
    
        scanf("%d", &k);
    
        for(i=k; i>=1; i--){
    
            for(j=i; j<=k; j++) {
    
         
            printf("%d ", j);
    
           }printf("
    ");
        }
    }

---
+++ Basic_Pattern_2.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,k;
+
+    scanf("%d", &k);
+
+    for(i=k; i>=1; i--){
+
+        for(j=i; j<=k; j++) {
+
+     
+        printf("%d ", j);
+
+       }printf("
+");
+    }
+}
